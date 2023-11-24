8f8b3321c7cca8e8e1e2483c3877c21fc0b1b3f2
Adrian Lagarto <adrianlagartomc@gmail.com>
2023-11-23 20:35:10 -0800

    set up the basic

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+
+function initializePage() {
+  loadHomePage();//creates main page for the todo list
+  tabs();//generates new tabs on page
+}
+
+
+
+/*
+  load the main page
+
+  the side bar there are options for
+  shows todo today list
+  shows all to list//Home
+    -Home
+    -Today
+    //Projects list
+    -list of projects
+    Notes
+  
+  
+*/
+
