0f5971da112ac563f63036a43f56d8d5918d8eb5
Adrian Lagarto <adrianlagartomc@gmail.com>
2023-11-27 15:56:33 -0800

    button pop up in form

--- src/addButton.js
+++ src/addButton.js
@@ -1,31 +1,89 @@
 
 
-
 function addButton (menu) {
   //task name, details, date, priority
+    
+
+  const container = document.querySelector('#container');
+
   const popUpBox = document.createElement('div');
   const title = document.createElement('input');
-  const details = document.createElement('input');
-  const priority = document.createElement('input');
+  const details = document.createElement('textarea');
+  const priority = document.createElement('div');
   const date = document.createElement('input');
   const confirm = document.createElement('button');
   const exit = document.createElement('button');
 
-  popUpBox.setAttribute('id', 'pop-up-box');
+  popUpBox.setAttribute('class', 'pop-up-box')
+  popUpBox.setAttribute('id', 'pop-up-box-1');
+  title.setAttribute('id', 'title');
+  details.setAttribute('id', 'details');
+  priority.setAttribute('id', 'priority');
+  date.setAttribute('id', 'date');
+  confirm.setAttribute('id', 'confirm');
+  exit.setAttribute('id', 'exit');
+
+  title.placeholder = 'Title';
+  details.placeholder = 'Details';
+  details.rows = '6';
+  details.cols = '50';
+
+
+  date.type = 'date';
+  confirm.textContent = 'Confirm';
+  exit.textContent = 'Exit';
 
     if(menu === 'all'){
       console.log('all');
-      title.placeholder = 'Title';
-      details.placeholder = 'Details';
-      details.type = 'range';
+
+      const form = document.createElement('form');
+      const priorityLow = document.createElement('input');
+      const priorityMedium = document.createElement('input');
+      const priorityHigh = document.createElement('input');
+      const low = document.createElement('label');
+      const medium = document.createElement('label');
+      const high = document.createElement('label');
+
+      priorityLow.type = 'radio';
+      priorityMedium.type = 'radio';
+      priorityHigh.type = 'radio';
+      priorityLow.name = 'priority';
+      priorityMedium.name = 'priority';
+      priorityHigh.name = 'priority';
+      low.textContent = 'low';
+      medium.textContent = 'medium';
+      high.textContent = 'high';
+
+      priorityLow.setAttribute('id', 'low-input');
+      priorityMedium.setAttribute('id', 'medium-input');
+      priorityHigh.setAttribute('id', 'high-input');
+
+      priorityLow.value = 'low';
+      priorityMedium.value = 'medium';
+      priorityHigh.value = 'high';
+
+      low.setAttribute('id', 'low');
+      medium.setAttribute('id', 'medium');
+      high.setAttribute('id', 'high');
 
 
-      popUpBox.appendChild(title);
-      popUpBox.appendChild(details);
-      popUpBox.appendChild(priority);
-      popUpBox.appendChild(date);
-      popUpBox.appendChild(confirm);
-      popUpBox.appendChild(exit);
+      form.appendChild(low);
+      form.appendChild(priorityLow);
+      form.appendChild(medium);
+      form.appendChild(priorityMedium);
+      form.appendChild(high);
+      form.appendChild(priorityHigh);
+
+      
+      form.appendChild(title);
+      form.appendChild(details);
+      form.appendChild(date);
+      form.appendChild(confirm);
+      form.appendChild(exit);
+
+      popUpBox.appendChild(form)
+      container.appendChild(popUpBox)
+      console.log(form)
     }
 }
 
@@ -35,14 +93,6 @@ function allTask (){
 
 }
 
-
-
-
-
-
-
-
-
-/*menu = tast for All/today, project, notes */
+/*menu = task for All/today, project, notes */
 
 export default addButton;
--- src/tabAll.js
+++ src/tabAll.js
@@ -1,13 +1,14 @@
 import addButton from "./addButton";
 
 const allTab = () => {
+  const allTaskList = [];
   const main = document.querySelector('#main');
   const pageContent = document.createElement('div')
   pageContent.classList.add('page-content');
   pageContent.textContent = 'All Tab!!!';
   console.log('works');
 
-  /*Add task button */
+  /*Add task button pop-up*/
   const addBtn = document.createElement('button');
   addBtn.textContent = '+'
   pageContent.appendChild(addBtn)
@@ -17,6 +18,19 @@ const allTab = () => {
 
   main.appendChild(pageContent);
 }
+
+
+  const pressConfirm = document.querySelector('#confirm');
+  const pressExit = document.querySelector('#exit');
+
+  pressConfirm.addEventListener('click', () => {
+    //gather the input data 
+  })
+
+  pressExit.addEventListener('click', () => {
+    //exit the pop up box
+  })
+  
 
 /*shows All the task for the project date sorted*/
 
